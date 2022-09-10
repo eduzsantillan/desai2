853e92b8f47336a2cb7325b65a93301444676e86
Edu Zuñiga <edu@MacBook-Pro.local>
2022-09-10 09:49:42 -0500

    Segunda sesion : jdbc para mantenimiento y prepared statements

--- src/main/java/com/isil/jdbc/MainApp.java
+++ src/main/java/com/isil/jdbc/MainApp.java
@@ -1,19 +1,13 @@
 package com.isil.jdbc;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MainApp {
 
     public static void main(String[] var) throws Exception {
 
-
-
         String jdbcUrl = "jdbc:mysql://localhost:3306/DESAI";
-
 
         //Cargar el driver
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -23,16 +17,41 @@ public class MainApp {
 
         //crear el statement
         Statement st = con.createStatement();
-
         //ejecutar la sentencia SQL
-        ResultSet rs = st.executeQuery("select * from Users");
+        //Consulta
+        //ResultSet rs = st.executeQuery("select * from Users");
 
+        //Insertar
+        //int filasAfectadas= st.executeUpdate("insert into Users values(1,'Juan','984444459','Vancuver')");
+
+        //Actualizar
+        //int filasAfectadas= st.executeUpdate("update Users set name='Christian' where name='Cueva'");
+
+        //Eliminar
+        //int filasAfectadas= st.executeUpdate("delete from Users where idUser=4");
+        //System.out.println("Filas afectadas: "+filasAfectadas);
+
+        //crear prepared statement
+        //Prepared statement para mantenimiento
+        PreparedStatement preparedStatement = con.prepareStatement("insert into Users values(?,?,?,?)");
+        preparedStatement.setInt(1, 8);
+        preparedStatement.setString(2, "Marta");
+        preparedStatement.setString(3,"985454646");
+        preparedStatement.setString(4,"Bogota");
+
+        int filasAfectadas= preparedStatement.executeUpdate();
+        System.out.println("Filas afectadas: "+filasAfectadas);
+
+        //Prepared statement para consulta
+        PreparedStatement preparedStatement2 = con.prepareStatement("SELECT * FROM USERS WHERE idUser=?");
+        preparedStatement2.setInt(1, 6);
+        ResultSet rs = preparedStatement2.executeQuery();
 
         //procesar el resultado
         while(rs.next()){
             System.out.println(rs.getString("idUser") + " " +
-                    rs.getString("name") + 
-                    " " + rs.getString("phone") + 
+                    rs.getString("name") +
+                    " " + rs.getString("phone") +
                     " " + rs.getString("city"));
         }
 
